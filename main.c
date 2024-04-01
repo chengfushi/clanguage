5737a02de3b6119804739e27dcf235bd485c77f2
shi-chengfu <202321073032@mail.scuec.edu.cn>
2024-04-01 17:09:36 +0800

    4月1日代码

---
+++ main.c
@@ -0,0 +1,74 @@
+///*测试ASCII码表*/
+//#include <stdio.h>
+//int main(void)
+//{
+////   循环输出ASCII码
+//    for(int i=32;i<128;i++){
+//        printf("%c",i);
+////        每16个字符换行
+//    if(i%16==15){
+//        printf("\n");
+//        }
+//    }
+//    return 0;
+//}
+/*a++和++a的区别*/
+#include<stdio.h>
+//int main(){
+//    int a=0;
+//    int b=++a;
+//    int c=a++;
+//    printf("%d\n",b);
+//    printf("%d\n",c);
+//    return 0;
+//}
+//#include <stdio.h>
+//int num = 10;
+//int main()
+//{
+//    int num = 1;
+//    printf("num = %d\n", num);
+//    return 0;
+//}
+#include <stdio.h>
+//int main()
+//{
+//    int a, b, c;
+//    a = 5;
+//    c = ++a;
+//    b = ++c, c++, ++a, a++;
+//    b += a++ + c;
+//    printf("a = %d b = %d c = %d\n:", a, b, c);
+//    return 0;
+//}
+/*负数求模*/
+#include <stdio.h>
+//int main(){
+//    printf("%d\n",11%5);
+//    printf("%d\n",-11%-5);
+//    printf("%d\n",11%-5);
+//    return 0;
+//}
+/*复合赋值*/
+//#include <stdio.h>
+//int main(){
+//    int a=0;
+//    a++;
+//    a=a+5;
+//    printf("%d",a);
+//    return 0;
+//}
+//#include <stdio.h>
+//int main(){
+//    int a=5,b;
+//    b=a--;
+//    printf("%d\n%d\n",a,b);
+//    return 0;
+//}
+/*强制类型转换*/
+//#include <stdio.h>
+//int main(){
+//    int a=(int)3.14;
+//    printf("%d",a);
+//    return  0;
+//}
