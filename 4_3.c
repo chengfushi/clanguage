fa80baa8fa9d4a716f8ce83e69909b1395834d86
shi-chengfu <202321073032@mail.scuec.edu.cn>
2024-04-03 18:14:28 +0800

    4月三日

---
+++ 4_3.c
@@ -0,0 +1,34 @@
+////测试scanf函数返回值
+#include <stdio.h>
+//int main(){
+//    int a,b,c;
+//    int r= scanf("%d%d%d",&a,&b,&c);
+//        printf("a=%d,b=%d,c=%d\n",a,b,c);
+//        printf("r=%d\n",r);
+//    return 0;
+//}
+//
+//scanf函数返回字符
+//int main(){
+//    char ch;
+//    scanf("%c",&ch);
+//    printf("%c\n",ch);
+//    return 0;
+//}
+//数组存储
+//%s字符串只会读取到第一个空白符之前
+//int main(){
+//    char a[10]={0};
+//    scanf("%s",a);//数组是个地址，不用&
+//    printf("%s\n",a);
+//    return 0;
+//}
+//if语句真假
+//int main(){
+//    int a=0;
+//    if(a){
+//        printf("hehe\n");
+//    }
+//    return 0;
+//}
+//
