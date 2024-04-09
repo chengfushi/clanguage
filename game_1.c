595f9dee0c4ecbb2f762e59b71fbe2b6e153892b
shi-chengfu <202321073032@mail.scuec.edu.cn>
2024-04-09 21:56:34 +0800

    猜数字游戏

---
+++ game_1.c
@@ -0,0 +1,66 @@
+//随机数游戏
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void mneu()
+{
+    printf("**********************\n");
+    printf("**********************\n");
+    printf("*******1.play*********\n");
+    printf("*******0.exit*********\n");
+    printf("**********************\n");
+    printf("**********************\n");
+}
+void game() {
+    //生成随机数
+    int r = rand() % 100 + 1;
+    int guess = 0;
+    //输入你猜的数字
+    printf("请输入一个数字");
+    scanf("%d", &guess);
+    //猜数字
+    while (1)
+    {
+        if (guess > r)
+        {
+            printf("猜大了\n");
+        } else if (guess < r)
+        {
+            printf("猜小了\n");
+        } else
+        {
+            printf("猜对了\n");
+            break;
+        }
+        //猜错了重新猜
+        printf("请重新输入一个数字\n");
+        scanf("%d",&guess);
+    }
+}
+int main()
+{
+    int input = 0;
+    //生成一个种子
+    srand((unsigned int)time(NULL));
+    do {
+        //背景页面
+        mneu();
+        printf("请选择：");
+        scanf("%d",&input);
+        switch (input)
+        {
+            //输入1玩游戏
+            case 1:
+                game();
+                break;
+            //输入2不玩
+            case 0:
+                printf("游戏结束");
+                break;
+            default:
+                printf("输入错误请重新输入");
+
+        }
+    }while(input);
+    return 0;
+}
