8ef97825e88c08598561e76a302b1319297389d8
shi-chengfu <202321073032@mail.scuec.edu.cn>
2024-04-23 11:36:41 +0800

    阶乘和

---
+++ jc.c
@@ -0,0 +1,19 @@
+//阶乘和
+#include "game.h"
+int roust(int x)
+{
+    if(x==0||x==1)
+        return 1;
+    return x* roust(x-1);
+}
+int main() {
+    int n = 0;
+    int m = 1;
+    int sum = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i)
+    {
+     sum+= roust(i);
+    }
+    printf("%d",sum);
+}
