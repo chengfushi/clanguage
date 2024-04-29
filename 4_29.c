48acf8762119ea5231ecc36b85789037eb0dd1ad
shi-chengfu <202321073032@mail.scuec.edu.cn>
2024-04-29 19:38:30 +0800

    指针

---
+++ 4_29.c
@@ -0,0 +1,71 @@
+
+//int main()
+//{
+//    char a = 100;
+//    char b = 20;
+//    char c = a + b;
+//    char d = 120;
+//    printf("%c\n",d);
+//    printf("%c\n",c);
+//    return 0;
+//}
+//int main()
+//{
+//    int i = 1;
+//    int j = 1;
+//    int c = (++i)+(++i)+(++i);
+//    int d = (j++)+(j++)+(j++);
+//    printf("%d\n",c);
+//    printf("%d\n",i);
+//    printf("%d\n",d);
+//    printf("%d\n",j);
+//    return 0;
+//int main()
+//{
+//    int a = 20;
+//    &a;
+//    printf("%p\n",&a);
+//    int *p = &a;
+//    * p=200;
+//    printf("%p\n",p);
+//    printf("%d\n",a);
+//    return 0;
+//}
+//int main()
+//{
+//    int a = 10;
+//    int* p = &a;
+//    printf("%d",sizeof (p));
+//    return 0;
+//}
+//用简单素数筛选法求N以内的素数。
+//判断一个数是不是素数
+#include <stdio.h>
+//int math(int x)
+//{
+//    int i =0;
+//    for(i = 2;i < x; i++)
+//    {
+//        if(x%i==0)
+//        {
+//            return 0;
+//        }
+//    }
+//    return x;
+//}
+//int main()
+//{
+//    int n = 0;
+//    //输入数字N
+//    scanf("%d",&n);
+//    int i =0;
+//    for(i = 2;i <= n; i++)
+//    {
+//        int key = math(i);
+//        if(key!=0)
+//        {
+//            printf("%d\n",key);
+//        }
+//    }
+//    return 0;
+//}
