7ddbff5d2ac3182b4bd405c146701508d976aed0
lanan <162813079+chengfushi@users.noreply.github.com>
2024-05-06 19:04:35 +0800

    Add files via upload

---
+++ 5_6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//int main(){
+//    int a = 10;
+//    int m =100;
+//    int const * const p = &a;
+//    printf("%d\n",*p);
+//    return 0;
+//}
+//指针访问数组
+//int main(){
+//    int a[10] = {0,1,2,3,4,5,6,7,8,9};
+//    int sz = sizeof (a)/sizeof (a[0]);
+//    int *p = &a[0];
+//    for (int i = 0; i < sz; ++i)
+//    {
+//        printf("%d\n",*(p+i));
+//    }
+//    return 0;
+//}
